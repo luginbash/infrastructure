611877b289ce04981e797b1234f8a3df9dbf8bd8
Luginbash Hiyajo <725033+luginbash@users.noreply.github.com>
2019-12-04 16:48:50 +0800

    Remove interpolation expressions

--- terraform/packet.tf
+++ terraform/packet.tf
@@ -3,15 +3,15 @@ variable "PACKET_PROJECT" {}
 
 
 provider packet {
-  auth_token = "${var.PACKET_AUTH_TOKEN}"
+  auth_token = var.PACKET_AUTH_TOKEN
 }
 
 
 locals {
-  project_id = "${var.PACKET_PROJECT}"
+  project_id = var.PACKET_PROJECT
 }
 
-data "packet_spot_market_price" "ams1" {
+data "packet_spot_market_price" "amsterdam" {
   facility = "ams1"
   plan     = "t1.small.x86"
 }
