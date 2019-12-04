b880e18531b3d4e3ec66daf66fe234628249c8c0
Luginbash Hiyajo <725033+luginbash@users.noreply.github.com>
2019-12-04 16:38:44 +0800

    Add heroku and packet

---
+++ terraform/packet.tf
@@ -0,0 +1,14 @@
+provider packet {
+  auth_token = "${var.PACKET_AUTH_TOKEN}"
+}
+
+
+locals {
+  project_id = "${var.PACKET_PROJECT}"
+}
+
+data "packet_spot_market_price" "ams1" {
+  facility = "ams1"
+  plan     = "t1.small.x86"
+}
+
