8ec646e8ad60c48471b5eab8f24f6888f2ff460a
Luginbash Hiyajo <725033+luginbash@users.noreply.github.com>
2019-12-25 11:05:35 +0800

    Add new device in ams, on packet.net

--- terraform/packet.tf
+++ terraform/packet.tf
@@ -11,5 +11,12 @@ locals {
   project_id = var.PACKET_PROJECT
 }
 
-
+resource "packet_device" "amstntpks01"{
+  hostname = "amstntpks01.lug.sh"
+  plan = "baremetal_0"
+  facilities = ["ams1"]
+  operating_system = "ubuntu_18_04"
+  billing_cycle    = "hourly"
+  project_id       = local.project_id
+}
 
