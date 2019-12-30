3b3adcaacdaa2baa673c64ca20a803033d8192cf
Luginbash Hiyajo <725033+luginbash@users.noreply.github.com>
2019-12-30 15:36:33 +0800

    Remove packet_devices for cost reason

--- terraform/packet.tf
+++ terraform/packet.tf
@@ -12,22 +12,4 @@ locals {
   project_id = var.PACKET_PROJECT
 }
 
-data "template_file" "init" {
-  template = file("${path.module}/init.tpl")
-  vars = {
-    master_addr = var.SALT_MASTER
-    master_finger = var.SALT_FINGER
-  }
-}
-
-resource "packet_device" "amstnlpks01" {
-  hostname = "amstnlpks01.lug.sh"
-  plan = "baremetal_0"
-  facilities = ["ams1"]
-  operating_system = "ubuntu_18_04"
-  billing_cycle = "hourly"
-  user_data = data.template_file.init.rendered
-  project_id = local.project_id
-}
-
 
