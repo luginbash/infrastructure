cfe417cf7437397daf457a34b84f30714aa63454
Luginbash Hiyajo <725033+luginbash@users.noreply.github.com>
2019-12-31 15:49:03 +0800

    Import CloudFlare resources

--- terraform/vultr.tf
+++ terraform/vultr.tf
@@ -1,16 +1,26 @@
 variable "VULTR_API_KEY" {}
+variable "SALT_MASTER" {}
+variable "SALT_FINGER" {}
+variable "SALT_USER" {}
 
 provider "vultr" {
   api_key =  var.VULTR_API_KEY
   rate_limit = 700
 }
 
+data "template_file" "script" {
+  template = file("${path.module}/init.tpl")
 
-data "vultr_plan" "hf12" {
-  filter {
-      name = "disk"
-      values = ["64"]
+  vars {
+    master_finger = var.SALT_FINGER
+    master_addr = var.SALT_MASTER
+    master_user = var.SALT_USER
   }
+}
+
+data "template_cloudinit_config" "vm-config" {
+  base64_encode = true
+  content      = "${data.template_file.script.rendered}"
 }
 
 resource "vultr_firewall_group" "untrust" {
@@ -23,7 +33,7 @@ resource "vultr_firewall_group" "default" {
 
 
 resource "vultr_server" "tokyjpvl01" {
-  plan_id = "401"
+  plan_id = "401" # HF12
   os_id = "252" # ubuntu1710
   region_id = "25" # Tokyo
 }
@@ -31,7 +41,16 @@ resource "vultr_server" "tokyjpvl01" {
 resource "vultr_server" "amstnlvl01" {
   plan_id = "400" # HF6
   os_id = "270" # ubuntu1710
-  region_id = "7" # Tokyo
+  region_id = "7" # Amsterdam
 }
 
-
+resource "vultr_server" "sngpsgvl01" {
+  label = "sngpsgvl01"
+  hostname = "sngpsgvl01.lug.sh"
+  enable_ipv6 = true
+  notify_activate = false
+  plan_id = "201" # Normal 5
+  os_id = "270" # Ubuntu 18.04
+  region_id = "40" # Singapore
+  user_data = data.template_cloudinit_config.vm-config.rendered
+}
