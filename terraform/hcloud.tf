538aee0e30f8b32d84169059b850cb95f8aa47cb
Luginbash Hiyajo <725033+luginbash@users.noreply.github.com>
2020-06-09 09:41:48 +0800

    ADD sk-ed25519 key to hetzner

--- terraform/hcloud.tf
+++ terraform/hcloud.tf
@@ -4,3 +4,19 @@ provider "hcloud" {
   token = var.hcloud_token
 }
 
+variable "SALT_MASTER" {}
+variable "SALT_FINGER" {}
+
+resource "hcloud_ssh_key" "openssh-sk" {
+  name = "SK-Test"
+  public_key = var.SSH_PUBKEY_1
+}
+
+resource "hcloud_server" "node1" {
+  name = "node1"
+  image = "ubuntu_2004"
+  server_type = "cx21"
+  location = "fsn1"
+  user_data = data.template_cloudinit_config.vm-config.rendered
+}
+
