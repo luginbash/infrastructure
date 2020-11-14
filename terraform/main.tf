23db6bd5263e46b51d279dc465ec3d85a65aaee1
Luginbash Hiyajo <725033+luginbash@users.noreply.github.com>
2020-11-14 12:17:28 +0800

    Remove Google

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,11 +3,7 @@ variable "SALT_MASTER" {}
 variable "SALT_FINGER" {}
 
 provider "google" {
-   project     = var.GOOGLE_CLOUD_PROJECT
-   region      = var.GOOGLE_CLOUD_REGION_DEFAULT
 }
-variable "GOOGLE_CLOUD_REGION_DEFAULT" {}
-variable "GOOGLE_CLOUD_PROJECT" {}
 
 
 data "template_file" "init" {
